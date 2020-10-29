dac819a0c7f55d9526a52858807a724ea764c4ab
Junghyun Gan <jhgan00@gmail.com>
2020-10-30 07:24:49 +0900

    deploy

--- core/settings.py
+++ core/settings.py
@@ -17,10 +17,10 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False)
+DEBUG = False #config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['0.0.0.0', '*', "https://mirae-289806.uc.r.appspot.com/"]
 
 # Application definition
 
